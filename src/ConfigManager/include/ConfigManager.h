481a81446ea17478f8fa00de46ff9c57523d4b79
Dannys <mutex_@outlook.com>
2025-12-06 02:26:15 +0800

    ConfigManager： 增加配置管理器库

---
+++ src/ConfigManager/include/ConfigManager.h
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace siren {
+namespace configManager {
+class ConfigManager {
+};
+}  // namespace configManager
+}  // namespace siren
--- test/testDLT645/testDLT645DataSheet.cc
+++ test/testDLT645/testDLT645DataSheet.cc
@@ -5,6 +5,9 @@
 namespace siren {
 namespace dlt645 {
 class TestDLT645DataSheet : ::testing::Test {
+public:
+  void SetUp() override {}
+  void TearDown() override {}
 };
 }  // namespace dlt645
 }  // namespace siren
